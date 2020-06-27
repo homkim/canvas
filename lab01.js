4522761402018df4c8ac1a72ec359b28994a2415
homkim <homkim01@gmail.com>
2020-06-27 11:09:46 +0900

    2nd change

---
+++ lab01.js
@@ -0,0 +1,11 @@
+
+function draw() {
+    var ctx = document.getElementById("myCanvas").getContext("2d");
+
+    
+    
+    
+    // var c = document.getElementById("myCanvas");
+    // var ctx = c.getContext("2d");
+
+}
